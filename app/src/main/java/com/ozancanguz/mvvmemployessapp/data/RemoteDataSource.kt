335b5b9481a08d23e70aa60b7fb699d8b94fdd1e
Ozan <ozancangu24@gmail.com>
2022-12-02 22:29:16 +0300

    -remote data source class modified

--- app/src/main/java/com/ozancanguz/mvvmemployessapp/data/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/mvvmemployessapp/data/RemoteDataSource.kt
@@ -1,4 +1,16 @@
 package com.ozancanguz.mvvmemployessapp.data
 
-class RemoteDataSource() {
+import com.ozancanguz.mvvmemployessapp.data.network.EmployeeApi
+import com.ozancanguz.mvvmemployessapp.models.Employee
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(private val employeeApi: EmployeeApi) {
+
+
+    suspend fun getEmployees(): Response<Employee> {
+        return employeeApi.getEmployees()
+    }
+
+
 }
